2deb21d74e73bcd7182972ee038dd6375d29a58b
nasreddine <nasreddinerahouni@gmail.com>
2025-03-21 22:14:37 +0000

    adding a promise based functionality to getElement method (module)

--- src/modules/dom_lib.js
+++ src/modules/dom_lib.js
@@ -4,24 +4,31 @@ export function isHTMLElement(element) {
 export function applyGetElement() {
     Node.prototype.getElement =
         function (elementIdentification, callback) {
-            let previousElement = this;
-            if (isHTMLElement(this.previousElement)) {
-                previousElement = this.previousElement
-            }
-            //  check if exists
-            if (previousElement.querySelector(elementIdentification)) {
-                callback?.(previousElement.querySelector(elementIdentification))
-                return previousElement.querySelector(elementIdentification)
-            }
-            // check for the element after each mutation in the document
-            let mutationObserver = new MutationObserver((mutations) => {
+            return new Promise((resolve, reject) => {
+                let previousElement = this;
+                if (isHTMLElement(this.previousElement)) {
+                    previousElement = this.previousElement
+                }
+                //  check if exists
                 if (previousElement.querySelector(elementIdentification)) {
                     callback?.(previousElement.querySelector(elementIdentification))
-                    mutationObserver.disconnect()
-                    return previousElement.querySelector(elementIdentification)
+                    resolve(previousElement.querySelector(elementIdentification))
+                    return
                 }
+                // check for the element after each mutation in the document
+                let mutationObserver = new MutationObserver((mutations) => {
+                    // console.log("looking for " + elementIdentification);
+
+                    if (previousElement.querySelector(elementIdentification)) {
+                        callback?.(previousElement.querySelector(elementIdentification))
+                        resolve(previousElement.querySelector(elementIdentification))
+                        mutationObserver.disconnect()
+                        // console.log("found " + elementIdentification);
+
+                    }
+                })
+                mutationObserver.observe(document, { subtree: true, childList: true, attributes: true })
             })
-            mutationObserver.observe(document, { subtree: true, childList: true, attributes: true })
+
         }
 }
-
